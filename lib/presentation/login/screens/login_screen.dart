af276f0dde07abc287428f40ddfeb83ef273ea05
NadaSaber2112 <nadasaber6668@gmail.com>
2025-11-24 23:02:10 +0200

    property detials feature

--- lib/presentation/login/screens/login_screen.dart
+++ lib/presentation/login/screens/login_screen.dart
@@ -97,40 +97,40 @@ class _LoginScreenState extends State<LoginScreen> {
                 ElevatedButton(
                   style: AppButtons.primary,
                   onPressed: () async {
-                    if (_formKey.currentState!.validate()) {
-                      final email = userValue.text.trim();
-                      final password = passValue.text.trim();
+                    // if (_formKey.currentState!.validate()) {
+                    //   final email = userValue.text.trim();
+                    //   final password = passValue.text.trim();
 
-                      final entity = LoginEntity(
-                        email: email,
-                        password: password,
-                      );
+                    //   final entity = LoginEntity(
+                    //     email: email,
+                    //     password: password,
+                    //   );
 
-                      try {
-                        final response = await repo.loginUser(entity);
+                    //   try {
+                    //     final response = await repo.loginUser(entity);
 
-                        await SharedHelper.saveToken(response.token);
-                        await SharedHelper.setLoggedIn(true);
+                    //     await SharedHelper.saveToken(response.token);
+                    //     await SharedHelper.setLoggedIn(true);
 
-                        if (!context.mounted) return;
+                    //     if (!context.mounted) return;
 
-                        Navigator.pushReplacementNamed(context, '/role').then((
-                          _,
-                        ) {
-                          userValue.clear();
-                          passValue.clear();
-                        });
-                      } catch (e) {
-                        if (!context.mounted) return;
+                    //     Navigator.pushReplacementNamed(context, '/role').then((
+                    //       _,
+                    //     ) {
+                    //       userValue.clear();
+                    //       passValue.clear();
+                    //     });
+                    //   } catch (e) {
+                    //     if (!context.mounted) return;
 
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          SnackBar(
+                    //     ScaffoldMessenger.of(context).showSnackBar(
+                    //       SnackBar(
                             
-                            content: Text(e.toString()),
-                          ),
-                        );
-                      }
-                    }
+                    //         content: Text(e.toString()),
+                    //       ),
+                    //     );
+                    //   }
+                    // }
                   },
                   child: const Text(
                     'Login',
