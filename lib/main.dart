75d974339668fdee5e7d3f3610534804394235b8
NadaSaber2112 <nadasaber6668@gmail.com>
2026-01-07 19:40:22 +0200

    main change add ScreenUtilInit widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:hive_flutter/hive_flutter.dart';
 import 'package:movin/data_injection/getIt/service_locator.dart';
 
 import 'package:movin/presentation/fav_screen/manager/fav_bloc/fav_bloc.dart';
 import 'package:movin/presentation/fav_screen/manager/fav_bloc/fav_event.dart';
-
 
 import 'package:movin/presentation/add_property/add_property_screen.dart';
 import 'package:movin/presentation/home/managers/mode_service.dart';
@@ -41,21 +41,26 @@ class Movin extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: const Splash(),
-      routes: {
-        '/onboarding': (_) => const OnboardingScreen(),
-        '/login': (_) => const LoginScreen(),
-        '/role': (_) => const RoleSelection(),
-        '/buyerhome': (_) => const BuyerHome(),
-        '/sellerhome': (_) => const SellerHome(),
-        '/forgotpassword': (_) => const ForgotPasswordPage(),
+    return ScreenUtilInit(
+      designSize: const Size(375, 812),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, __) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          home: const Splash(),
+          routes: {
+            '/onboarding': (_) => const OnboardingScreen(),
+            '/login': (_) => const LoginScreen(),
+            '/role': (_) => const RoleSelection(),
+            '/buyerhome': (_) => const BuyerHome(),
+            '/sellerhome': (_) => const SellerHome(),
+            '/forgotpassword': (_) => const ForgotPasswordPage(),
 
-        '/home': (_) => const HomePage(),
-        '/addproperty': (_) => const AddPropertyScreen(),
-
-        
+            '/home': (_) => const HomePage(),
+            '/addproperty': (_) => const AddPropertyScreen(),
+          },
+        );
       },
     );
   }
