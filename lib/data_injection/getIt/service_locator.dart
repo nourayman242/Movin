3a28ce77eeedbeabab4b6fb54f93f1bb886471d4
NadaSaber2112 <nadasaber6688@gmail.com>
2026-04-12 20:25:21 +0200

    remove commit

--- lib/data_injection/getIt/service_locator.dart
+++ lib/data_injection/getIt/service_locator.dart
@@ -1,10 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import 'package:movin/data/api_services/google_auth_service.dart';
-import 'package:movin/data/repositories/auth_repository_impl.dart';
-import 'package:movin/domain/repositories/auth_repository.dart';
 import 'package:movin/presentation/fav_screen/manager/fav_hive.dart';
-import 'package:movin/presentation/login/cubit/auth_cubit.dart';
 import 'service_locator.config.dart';
 
 
@@ -17,20 +13,4 @@ Future<void> setUpServiceLocator() async {
   final favHive = getIt<FavoriteHiveService>();
   await favHive.init();
 
-  // Register Google service
-  // getIt.registerLazySingleton<GoogleAuthService>(
-  //   () => GoogleAuthService(),
-  // );
-
-  // Register repository
-  // getIt.registerLazySingleton<AuthRepository>(
-  //   () => AuthRepositoryImpl(
-  //     getIt<GoogleAuthService>(),
-  //   ),
-  // );
-
-  // Register cubit
-  // getIt.registerFactory<AuthCubit>(
-  //   () => AuthCubit(getIt<AuthRepository>()),
-  // );
 }
