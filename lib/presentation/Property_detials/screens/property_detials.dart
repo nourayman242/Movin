464bf0b0d533e6fb4365462eed11cc02144c0e60
NadaSaber2112 <nadasaber6688@gmail.com>
2026-05-02 16:31:40 +0300

    remove comment from proparty detials

--- lib/presentation/Property_detials/screens/property_detials.dart
+++ lib/presentation/Property_detials/screens/property_detials.dart
@@ -12,69 +12,6 @@ import 'package:movin/presentation/home/widgets/property/tabs/property_tabs.dart
 import 'package:movin/presentation/home/widgets/property/title_card.dart';
 import 'package:movin/presentation/seller_properties/cubit/property_cubit.dart';
 
-// class PropertyDetailsScreen extends StatefulWidget {
-//   final PropertyEntity property;
-
-//   const PropertyDetailsScreen({super.key, required this.property});
-
-//   @override
-//   State<PropertyDetailsScreen> createState() => _PropertyDetailsScreenState();
-// }
-
-// class _PropertyDetailsScreenState extends State<PropertyDetailsScreen> {
-//   final controller = PropertyDetailsController();
-
-//   @override
-//   void initState() {
-//     super.initState();
-//     controller.init();
-//   }
-
-//   @override
-//   void dispose() {
-//     controller.dispose();
-//     super.dispose();
-//   }
-
-//   @override
-//   Widget build(BuildContext context) {
-//     ScreenUtil.init(context, designSize: const Size(390, 844));
-
-//     return Scaffold(
-//       backgroundColor: const Color(0xFFF7F7F7),
-//       body: SingleChildScrollView(
-//         child: AnimatedBuilder(
-//           animation: controller,
-//           builder: (context, _) {
-//             return Column(
-//               crossAxisAlignment: CrossAxisAlignment.start,
-//               children: [
-//                 PropertyImageSlider(
-//                   controller: controller.pageController,
-//                   property: widget.property,
-//                 ),
-//                 SizedBox(height: 16.h),
-//                 TitleCard(controller: controller, property: widget.property),
-//                 SizedBox(height: 16.h),
-//                 PropertyTabs(controller: controller),
-//                 SizedBox(height: 16.h),
-//                 if (widget.property.isAuction)
-//                   BlocProvider(
-//                     create: (context) =>
-//                         getIt<AuctionCubit>()..init(widget.property.id),
-//                     child: AuctionCard(property: widget.property),
-//                   ),
-//                 SizedBox(height: 16.h),
-//                 AgentCard(controller: controller),
-//                 SizedBox(height: 40.h),
-//               ],
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
 class PropertyDetailsScreen extends StatefulWidget {
   final String propertyId;
   const PropertyDetailsScreen({super.key, required this.propertyId});
